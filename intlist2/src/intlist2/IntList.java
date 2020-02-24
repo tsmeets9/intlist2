967c66751ba761a493d4d9f1ed6b3dbb3e270a40
tsmeets9 <47253276+tsmeets9@users.noreply.github.com>
2020-02-24 18:15:10 +0100

    Initial commit

---
+++ intlist2/src/intlist2/IntList.java
@@ -0,0 +1,24 @@
+package intlist2;
+
+class Node {
+	int element;
+	Node next;
+}
+public class IntList {
+
+	public int getSize() {
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	public int get(int index) {
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	public IntList() {
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	public void add(int element) {
+		throw new RuntimeException("Not yet implemented");
+	}
+}
